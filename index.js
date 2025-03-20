0010e6a0efdd4d9f5d1ffba6af32314a62b26de4
Bongozz <zlegaya@gmail.com>
2025-03-20 16:57:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const menuLink = document.getElementsByClassName("menuLink");
 document.addEventListener('DOMContentLoaded', function() {
 
 miniMe.addEventListener("click", event =>{
-    miniMe.src = 'img/me_ouch.jpg';
+    miniMe.src = 'https://piskel-imgstore-b.appspot.com/img/beceaab3-05e6-11f0-a390-adb355fe7ec4.gif';
     setTimeout(() => {
-        miniMe.src = 'img/me_owie.jpg';
+        miniMe.src = 'ihttps://piskel-imgstore-b.appspot.com/img/d8614335-05e6-11f0-a472-adb355fe7ec4.gif';
     }, 400);
 });
 
 miniMe.addEventListener("mouseover", event =>{
-    miniMe.src = 'img/me_hover.jpg';
+    miniMe.src = 'https://piskel-imgstore-b.appspot.com/img/fc5aedae-05e5-11f0-a58b-adb355fe7ec4.gif';
 });
 miniMe.addEventListener("mouseout", event =>{
     miniMe.src = 'https://piskel-imgstore-b.appspot.com/img/67f36deb-05e6-11f0-8b31-adb355fe7ec4.gif';
 });
 
 menuTitle.addEventListener("click", event =>{
-    miniMe.src = 'img/me_linkclick.jpg';
+    miniMe.src = 'https://piskel-imgstore-b.appspot.com/img/fc5aedae-05e5-11f0-a58b-adb355fe7ec4.gif';
     setTimeout(() => {
         miniMe.src = 'https://piskel-imgstore-b.appspot.com/img/67f36deb-05e6-11f0-8b31-adb355fe7ec4.gif';
     }, 400);
@@ -27,7 +27,7 @@ menuTitle.addEventListener("click", event =>{
 
 for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].addEventListener("mouseover", event => {
-        miniMe.src = 'img/me_intrigued.jpg';
+        miniMe.src = 'https://piskel-imgstore-b.appspot.com/img/f627577a-05e6-11f0-bac0-adb355fe7ec4.gif';
     });
 
     menuLink[i].addEventListener("mouseout", event => {
